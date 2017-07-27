f768830b82681ff5622a7fb8c59db21bb798a36e
Mussie <nmussie@LAMU02T625RGTFM.uhc.com>
2017-07-27 15:02:27 -0400

    hooked up to our page

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ var client = new elasticsearch.Client({
   log: 'trace'
 });
 
+$(document).ready(function(){
+  client.ping({
+    requestTimeout: 30000,
+  }, function (error) {
+    if (error) {
+      console.error('elasticsearch cluster is down!');
+    } else {
+      console.log('All is well');
+    }
+  });
 
-client.ping({
-  requestTimeout: 30000,
-}, function (error) {
-  if (error) {
-    console.error('elasticsearch cluster is down!');
-  } else {
-    console.log('All is well');
-  }
-});
-
-
-client.search({
-  q: 'dilbert'
-}).then(function (body) {
-  var hits = body.hits.hits;
-}, function (error) {
-  console.trace(error.message);
+  $("#submitSearch").click(function() {
+    client.search({
+      q: $("#search").val();
+    }).then(function (body) {
+      var hits = body.hits.hits;
+    }, function (error) {
+      console.trace(error.message);
+    });
+  });
 });
