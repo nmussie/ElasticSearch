c2e7a4b0e42e11f85c0fdbd978168b68d30aab6a
Nathan Mussie <Spiff@Nathans-MacBook-Pro.local>
2017-07-27 23:09:28 -0400

    minor bug fixes

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ var client = new $.es.Client({
 
 $(document).ready(function() {
   $("#submitSearch").click(function() {
-    $("#searchResults").empty();
     var val = $("#search").val();
-    search(val);
-    $("#search").val("");
+    if (val) {
+      $("#searchResults").empty();
+      search(val);
+      $("#search").val("");
+    }
   });
+  $("#search").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#submitSearch").click();
+    }
+});
 });
 
 function search(val) {
@@ -40,14 +47,16 @@ function showResult(result) {
     var date = result[i]._source["postDate"];
     var title = result[i]._source["title"];
     var user = result[i]._source["user"];
-    $("#searchResults").append('<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' +
-       '<div class="d-flex w-100 justify-content-between">' +
-         '<small class="text-muted pull-right">' + date + '</small>' +
-         '<h5 class="mb-1">' + title + '</h5></div>' +
-       '<p class="mb-1">' + body + '</p>' +
-       '<small class="text-muted">' + user + '</small></a>');
+    if (body && date && title && user) {
+      $("#searchResults").append('<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">' +
+         '<div class="d-flex w-100 justify-content-between">' +
+           '<small class="text-muted pull-right">' + date + '</small>' +
+           '<h5 class="mb-1">' + title + '</h5></div>' +
+         '<p class="mb-1">' + body + '</p>' +
+         '<small class="text-muted">' + user + '</small></a>');
+         $("#searchResults").show();
+    }
   }
-  $("#searchResults").show();
 }
 
 // function insertRow(date, title, body, user) {
