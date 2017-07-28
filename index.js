b36ff59e9a7f0eaf39722707a5dc88982c332cb4
Yang <sgyang9@gmail.com>
2017-07-27 20:07:13 -0400

    client side

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-// var elasticsearch = require('elasticsearch');
+//var elasticsearch = require('elasticsearch');
+//const $ = require('jquery');
 
 $(document).ready(function() {
   $("#submitSearch").click(function() {
     var val = $("#search").val();
+    console.log(val);
     console.log(search(val));
   });
 });
 
+// var client = new elasticsearch.Client({
+//   host: 'localhost:9200',
+//   log: 'trace'
+// });
+
+var client = new $.es.Client({
+  hosts: 'http://localhost:9200'
+});
+
+
 function search(val) {
-  var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
-  });
+  // var client = new $.es.Client({
+  //   host: 'localhost:9200'
+  // });
+
+  // var client = new elasticsearch.Client({
+  //   host: 'localhost:9200',
+  //   log: 'trace'
+  // });
 
   client.ping({
-    requestTimeout: 30000,
+    requestTimeout: 3000000,
   }, function (error) {
     if (error) {
       console.error('elasticsearch cluster is down!');
@@ -24,7 +40,7 @@ function search(val) {
   });
 
   client.search({
-    q: val
+    q: 'dilbert'
   }).then(function (body) {
     var hits = body.hits.hits;
   }, function (error) {
